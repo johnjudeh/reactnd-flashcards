a8b110cbb58d7022c5890e84997d6f478b651ad8
John Judeh <johnjudeh@hotmail.com>
2020-05-17 22:57:57 +0100

    feat: Flesh out the New Deck component

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { white } from '../constants/colors';
 
 function NewDeck(props) {
     return (
         <View>
             <Text>New Deck</Text>
+            <Text>Deck Title</Text>
+            <TextInput
+                style={styles.textInput}
+                onChangeText={() => console.log('hi')}
+            />
+            <TouchableOpacity>
+                <Text>Create Deck</Text>
+            </TouchableOpacity>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    textInput: {
+        height: 40,
+        backgroundColor: white,
+    }
+})
 
 export default NewDeck;
